f0b232f80ed2b7cb51caa59d53152ca83e2d1095
Anshika Kulshrestha <anshika.kulshrestha_da18@gla.ac.in>
2020-03-28 12:37:41 +0530

    update

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,7 +5,32 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
+import java.util.Scanner;
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 
 public class MyMain {
+ public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        System.out.print("For inserting data in BST press1, for printing left nodes and nodes which who doesn't have left child press2:- ");
+        int counter = sc.nextInt();
+        while (counter != 0) {
+            switch (counter) {
+                case 1:
+                    System.out.print("Enter number:- ");
+                    bst.insert(sc.nextInt());
+                    break;
+                case 2:
+                    bst.traverseLevelOrder(bst.getRoot());
+                    break;
+                default:
+                    System.out.println("Please enter a valid option!!!");
+            }
+            System.out.print("For inserting data in BST press1, for printing left nodes and nodes which who doesn't have left child press2, for exit press0:- ");
+            counter = sc.nextInt();
+        }
+        System.out.println("Thank you. Have a nice day!");
+    }
 }
